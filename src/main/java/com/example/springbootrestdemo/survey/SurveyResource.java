e108a4d576a60d3c0c389fe6136f1b0eac972fb6
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-09-22 21:02:32 +0530

    added the surveys end point

---
+++ src/main/java/com/example/springbootrestdemo/survey/SurveyResource.java
@@ -0,0 +1,25 @@
+package com.example.springbootrestdemo.survey;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class SurveyResource {
+
+
+
+    private SurveyService surveyService;
+
+    public SurveyResource(SurveyService surveyService) {
+        super();
+        this.surveyService = surveyService;
+    }
+
+    @RequestMapping("/surveys")
+    public List<Survey>  retrieveAllSurveys(){
+        return surveyService.retrieveAllSurveys();
+
+    }
+}
