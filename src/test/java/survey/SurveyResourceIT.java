5a2b26ef3ce3561fda9081af71637f9c92369bb9
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-09-25 17:45:29 +0530

    added the post junit teest

--- src/test/java/survey/SurveyResourceIT.java
+++ src/test/java/survey/SurveyResourceIT.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT,classes = SpringBootRestDemoApplication.class)
@@ -18,7 +21,6 @@ public class SurveyResourceIT {
 
     //http://localhost:8080/surveys/Survey1/questions/Question1
     String str = """
-			
 			{
 			  "id": "Question1",
 			  "description": "Most Popular Cloud Platform Today",
@@ -96,6 +98,46 @@ public class SurveyResourceIT {
 		JSONAssert.assertEquals(expectedResponse, responseEntity.getBody(), false);
 
 	}
+
+	@Test
+	void addNewSurveyQuestion_basicScenario() {
+
+		String requestBody = """
+				{
+				  "description": "Your Favorite Language",
+				  "options": [
+				    "Java",
+				    "Python",
+				    "JavaScript",
+				    "Haskell"
+				  ],
+				  "correctAnswer": "Java"
+				}
+			""";
+
+
+		//
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json");
+
+		HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody, headers);
+
+		ResponseEntity<String> responseEntity
+				= template.exchange(GENERIC_QUESTIONS_URL, HttpMethod.POST, httpEntity, String.class);
+
+		assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+
+		String locationHeader = responseEntity.getHeaders().get("Location").get(0);
+		assertTrue(locationHeader.contains("/surveys/Survey1/questions/"));
+
+		//DELETE
+		//locationHeader
+		//REMOVE SIDE EFFECT
+		template.delete(locationHeader);
+
+	}
+
 
 
 }
